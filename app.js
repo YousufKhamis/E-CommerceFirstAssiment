b766e2ec80c6fd3caa8601d3a4950e5d8f9f5f27
Yousuf Khamis <124306502+YousufKhamis@users.noreply.github.com>
2024-03-06 00:37:43 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,51 @@
+//CONTROL SEARCH BOX BEHAVIOR
+const searchInput = document.querySelector(".search-input");
+const searchBox = document.getElementById("search-box");
+
+// Function to update styles based on whether input value exists or not
+function updateStyles() {
+  if (searchInput.value.trim() !== "") {
+    // Apply styles when input is not empty
+    searchBox.style.width = "15rem";
+  } else {
+    // Reset styles when input is empty
+    searchBox.style.width = "2.1rem";
+  }
+}
+
+// Initial check
+updateStyles();
+
+// Listen for input changes
+searchInput.addEventListener("input", updateStyles);
+
+
+
+
+//CONTROL SLIDE
+const wrapper = document.querySelector(".sliderWrapper");
+const slides = document.querySelectorAll(".slide");
+const prevBtn = document.querySelector(".prevBtn");
+const nextBtn = document.querySelector(".nextBtn");
+let currentSlide = 0;
+
+function showSlide() {
+  slides.forEach((slide, i) => {
+    slide.style.transform = `translateX(-${currentSlide * 90}rem)`;
+  });
+}
+
+function nextSlide() {
+  currentSlide = (currentSlide + 1) % slides.length;
+  showSlide();
+}
+
+function prevSlide() {
+  currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+  showSlide();
+}
+
+showSlide();
+
+prevBtn.addEventListener("click", prevSlide);
+nextBtn.addEventListener("click", nextSlide);
